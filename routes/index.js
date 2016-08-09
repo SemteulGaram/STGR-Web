cd65b8d2d43ba24c8d2835d25302d1a5ede9c82c
SemteulGaram <scgtdy7151@gmail.com>
2016-08-09 14:16:42 +0900

    some analyser

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'SemteulGaram', analyse: "===request===\n" +
+  analyser(req) + "===response===\n" + analyer(res)});
 });
+
+function analyser(obj) {
+  if(typeof obj !== "object") return obj + "";
+  let result = "";
+  obj.map(function(v, i) {result += (i + ": " + v + "\n")});
+  return result;
+}
 
 module.exports = router;
