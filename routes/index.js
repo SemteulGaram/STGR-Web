5a5d629edfc062f2ce52fc16d252d3cff85cd7d9
SemteulGaram <scgtdy7151@gmail.com>
2016-08-13 03:15:48 +0900

    oops

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'SemteulGaram', analyse: "===request===\n" +
-  analyserParser(analyser(req, "request", 5)).split("\n") + "===response===\n" + analyserParser(analyser(res, "response", 5)).split("\n")});
+  res.render('index', {title: 'SemteulGaram',
+  analyserRequest: analyserParser(analyser(req, "request", 5)).split("\n"),
+  analyserResponse: analyserParser(analyser(res, "response", 5)).split("\n")});
 });
 
 function ObjectInfo(type, name, obj) {
